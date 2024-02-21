203e15a250d9363e98f6fe93f2a0eb4b89494316
Maxim Dubakin <maximdubakin@yandex-team.ru>
2024-02-21 09:53:33 +0400

    lessons 4.9 is done

---
+++ 04. Встроенные функции/4.9. Функции даты и времени. Часть 4/tasks/4.9.12.sql
@@ -0,0 +1,9 @@
+SELECT
+    name,
+    surname,
+    DATE_FORMAT(birth_date, "%d.%m.%Y") AS birth_date,
+    TIME_FORMAT(birth_time, "%H:%i") AS birth_time
+FROM
+    `Actors`
+WHERE
+    DAY(birth_date) <= 14;
---
+++ 04. Встроенные функции/4.9. Функции даты и времени. Часть 4/tasks/4.9.13.sql
@@ -0,0 +1,12 @@
+SELECT
+    name,
+    surname,
+    DATE_FORMAT(
+        TIMESTAMP(birth_date, birth_time),
+        "%d %M %Y, %H:%i"
+    ) AS birth_datetime
+FROM
+    `Actors`
+WHERE
+    YEAR(birth_date) BETWEEN 1969
+    AND 1975;
